9e30ce2b4e90091b5afb4b5b8d45b148538b0e0f
nathaliagodoymachado-star <nathalia.godoy.machado@escola.pr.gov.br>
2026-09-10 20:13:22 -0300

    ponto e virgula

--- main.js
+++ main.js
@@ -25,7 +25,7 @@ function aumentaTamanho() {
 const campoSenha = document.querySelector('#campo-senha');
 const checkbox = document.querySelectorAll('.checkbox');
 
-for (i = 0, i < checkbox.length; i++) {
+for (i = 0; i < checkbox.length; i++) {
     checkbox[i].onclick = geraSenha;
 }
 
